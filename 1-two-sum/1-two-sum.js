97dedff35a1b158c7380bbc672089e4e30098b8f
Jaseela.A <96909251+Jaseela99@users.noreply.github.com>
2022-08-12 00:05:57 +0530

    Time: 212 ms (19.27%), Space: 42.7 MB (52.87%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let arr=[]
+    for(let i=0;i<nums.length;i++){
+       
+        if(arr.includes(nums[i])){
+            return [i,arr.indexOf(nums[i])]
+        }else{
+            arr.push(target-nums[i])
+        }
+    }
+};
